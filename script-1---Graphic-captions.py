3e005dd6d8587b7b2b673e46a15e1fc6a3ebca3e
wiiiktor <41764319+wiiiktor@users.noreply.github.com>
2023-09-13 23:15:53 +0200

    Update script-1---Graphic-captions.py
    removing some unnecessary lines, added example output

--- script-1---Graphic-captions.py
+++ script-1---Graphic-captions.py
@@ -25,13 +25,6 @@ def get_files_from_dir(dir_path: str):
             res.append(dir_path + file_path)
     return res
 
-class RawDocument(BaseModel):
-    page_content: str
-    metadata: dict
-
-def convert_text_to_page_content_format(text):
-    return RawDocument(page_content=text, metadata={})
-
 
 DOCX_PATH = './files/docx/'
 
@@ -41,12 +34,6 @@ pprint(list_of_files, width = 200)
 chat = ChatOpenAI(temperature=0, model_name="gpt-4", max_tokens=700)
 
 text = read_docx(list_of_files[0])
-print(f"Długość załadowanego pliku w znakach: {len(text)}, w słowach: {len(text.split())}")
-text = convert_text_to_page_content_format(text)
-
-
-if isinstance(text, RawDocument):
-    text = text.page_content
 
 num_graphics = text.count("graphic-number")
 
@@ -70,3 +57,45 @@ prompt = PromptTemplate(
 chain = LLMChain(llm = chat, prompt = prompt)
 captions = chain.run(document = text, num_graphics = num_graphics)
 pprint(captions, width=150)
+
+'''
+example output: 
+('("graphic-number-001": "This graphic likely illustrates the tools needed for the installation process, as it is mentioned after the list of '
+ 'required tools.")\n'
+ '("graphic-number-002": "This graphic likely shows how to position the bike for the installation process, as it is mentioned in the context of '
+ 'positioning the bike with wheels upwards.")\n'
+ '("graphic-number-003": "This graphic likely demonstrates the process of removing front cranks from the bike, as it is mentioned in the context of '
+ 'crank and front derailleur removal.")\n'
+ '("graphic-number-004": "This graphic likely shows the bottom bracket that comes with the GTRO, as it is mentioned in the context of bottom bracket '
+ 'replacement.")\n'
+ '("graphic-number-005": "This graphic likely shows the positioning of the wave-spring in the bottom bracket’s niche, as it is mentioned in the '
+ 'context of gearbox installation.")\n'
+ '("graphic-number-006": "This graphic likely shows the positioning of the wave-spring in the bottom bracket’s niche, as it is mentioned in the '
+ 'context of gearbox installation.")\n'
+ '("graphic-number-007": "This graphic likely shows the positioning of the reaction lever on the chainstay, as it is mentioned in the context of '
+ 'gearbox installation.")\n'
+ '("graphic-number-008": "This graphic likely shows the correct positioning of the lever against the straight area of the chainstay or kickstand '
+ 'plate, as it is mentioned in the context of gearbox installation.")\n'
+ '("graphic-number-009": "This graphic likely shows the incorrect positioning of the lever against the edge of the kickstand’s plate, as it is '
+ 'mentioned in the context of gearbox installation.")\n'
+ '("graphic-number-010": "This graphic likely shows the incorrect positioning of the lever against the edge of the kickstand’s plate, as it is '
+ 'mentioned in the context of gearbox installation.")\n'
+ '("graphic-number-011": "This graphic likely shows the correct positioning of the shifting cable against the down tube, as it is mentioned in the '
+ 'context of gearbox installation.")\n'
+ '("graphic-number-012": "This graphic likely shows the correct positioning of the reaction lever against the chainstay, as it is mentioned in the '
+ 'context of gearbox installation.")\n'
+ '("graphic-number-013": "This graphic likely shows the installation of the cable-slider, as it is mentioned in the context of cable-slider '
+ 'installation.")\n'
+ '("graphic-number-014": "This graphic likely shows the installation of the GTRO’s shifter on the handlebar, as it is mentioned in the context of '
+ 'shifter installation.")\n'
+ '("graphic-number-015": "This graphic likely shows the installation of the GTRO’s shifter on the handlebar, as it is mentioned in the context of '
+ 'shifter installation.")\n'
+ '("graphic-number-016": "This graphic likely shows the installation of the GTRO’s shifter on the handlebar, as it is mentioned in the context of '
+ 'shifter installation.")\n'
+ '("graphic-number-017": "This graphic likely shows the installation of the lever to the pedal boom for bicycles with no chainstay, as it is '
+ 'mentioned in the context of no-chainstay bicycles.")\n'
+ '("graphic-number-018": "This graphic likely shows the application of grease under the plastic cap of the crank bolt for usage in wet/humid areas, '
+ 'as it is mentioned in the context of GTRO usage in highly wet/humid area.")\n'
+ '("graphic-number-019": "This graphic likely shows the installation of bolts for the adapter, as it is mentioned in the context of adapter for '
+ 'beltring or larger chainring.")')
+'''
