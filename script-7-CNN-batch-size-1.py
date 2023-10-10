89d54d03498171c2ae100a52c2f0f978da734f53
wiiiktor <41764319+wiiiktor@users.noreply.github.com>
2023-10-10 08:14:54 +0200

    Update script-7-CNN-batch-size-1.py
    po audycie

--- script-7-CNN-batch-size-1.py
+++ script-7-CNN-batch-size-1.py
@@ -14,64 +14,46 @@ import math
 from torchvision.transforms import RandomRotation
 from PIL import Image
 
-# fixing random seed, set to True only during development, otherwise you will always get the same results
-if False:
-    torch.manual_seed(0)
-    # random.seed(0)
-    np.random.seed(0)
-    torch.backends.cudnn.benchmark = False
-    # torch.use_deterministic_algorithms(True)
-    # dataloader also may require fixing
-torch.autograd.set_detect_anomaly(False)  # set to True only for debugging
 
-# region datasets
-train_dataset_50 = datasets.ImageFolder(
-    'data/imagenet_50_train/imagenet_images',
-    transforms.Compose([
+def create_dataset(path):
+    transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
-    ]))
+    ])
+    return datasets.ImageFolder(path, transform)
 
-val_dataset_50 = datasets.ImageFolder(
-    'data/imagenet_50_val/imagenet_images',
-    transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ]))
-# endregion
+train_dataset = create_dataset('data/imagenet_50_train/imagenet_images')
+val_dataset = create_dataset('data/imagenet_50_val/imagenet_images')
 
-n_epochs = 300
 batch_size = 1
-learning_rate = 0.005
-save = True
-load = False
-modelName = 'data/script-1---saved-model'
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-print('batch_size:', batch_size, 'lr:', learning_rate)
-print('training dataset length:', len(train_dataset_50))
-print('validatin dataset length:', len(val_dataset_50), '\n')
+print(f'batch_size: {batch_size} | lr: {0.005}')
+print(f'training dataset length: {len(train_dataset)}')
+print(f'validation dataset length: {len(val_dataset)}\n')
 
-train_dataloader = torch.utils.data.DataLoader(train_dataset_50, batch_size=batch_size, shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val_dataset_50, batch_size=batch_size, shuffle=True)
 
 class Conv2dWS(nn.Conv2d):
-    def __init__(self, in_channels=512, out_channels=512, kernel_size=(3, 3), stride=(1, 1),
-                 padding=(1, 1), dilation=1, groups=1, bias=True):
-        super(Conv2dWS, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+    def __init__(self, *args, **kwargs):
+        super(Conv2dWS, self).__init__(*args, **kwargs)
 
     def forward(self, x):
         weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
-        weight = weight - weight_mean
-        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
-        weight = weight / std.expand_as(weight)
-        return F.conv2d(x, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+        # Compute mean and subtract it from the weight
+        weight_mean = weight.mean(dim=(1, 2, 3), keepdim=True)
+        weight_centralized = weight - weight_mean
+
+        # Compute the standardized weight
+        std = weight_centralized.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
+        weight_standardized = weight_centralized / std.expand_as(weight)
+
+        return F.conv2d(x, weight_standardized, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
 
 class NewVGG(nn.Module):
     def __init__(self):
@@ -137,86 +119,76 @@ class NewVGG(nn.Module):
 
 
 ''' Model training, on 50 classes '''
-model = NewVGG().to('cuda:0')
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adadelta(model.parameters(), lr=learning_rate)
-exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
-""" Loading model """
-if load: model.load_state_dict(torch.load(modelName)); print("Model loaded")
+device = torch.device("cuda:0")
 
-print('Epochs number:', n_epochs)
-start_time = time.time()
-scaler = torch.cuda.amp.GradScaler()  # Automatic Mixed Precision package, torch.cuda.amp, to save memory & time
-for epoch in range(0, n_epochs):
+model = NewVGG().to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate)
+exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+scaler = torch.cuda.amp.GradScaler()
+
+if load:
+    model.load_state_dict(torch.load(modelName))
+    print("Model loaded")
+
+def train_one_epoch(model, dataloader, optimizer, criterion, scaler, batch_multiplier=20):
+    model.train()
     train_loss = 0.0
-    valid_loss = 0.0
-
-    start_epoch = time.time()
-
-    """ Training """
-    model.train(); print('Training start', '\t %4.1fm' % (time.time() / 60), '\t Epoch:', epoch + 1)
-
     counter = 0
-    batch_multiplier = 20
-    for data, target in train_dataloader:
-        counter = counter + 1
-        data, target = data.cuda(), target.cuda()
+    for data, target in dataloader:
+        counter += 1
+        data, target = data.to(device), target.to(device)
         with torch.cuda.amp.autocast():
             output = model(data)
-            output.cuda()
         loss = criterion(output, target)
         scaler.scale(loss).backward()
-        if counter % 100 == 0:  # just displaying something to show that application works ;)
-            print('counter1', counter, '\t %4.1fm' % (time.time()/60))
-        if counter % batch_multiplier == 0:  # solution that lowers memory consumption
+
+        if counter % batch_multiplier == 0:
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
         train_loss += loss.item() * data.size(0)
-        if counter % 7200 == 0 and save:  # saving model
-            torch.save(model.state_dict(), modelName); print("Model saved")
+
+        if counter % 7200 == 0 and save:
+            torch.save(model.state_dict(), modelName)
+            print("Model saved during epoch")
+
+    return train_loss
+
+def validate(model, dataloader, criterion):
+    model.eval()
+    valid_loss = 0.0
+    good1, good5 = 0, 0
+    for data, target in dataloader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        valid_loss += criterion(output, target).item() * data.size(0)
+
+        _, top5_preds = torch.topk(output, 5)
+        correct_tensor = top5_preds[0, 0].eq(target.data.view_as(top5_preds[0, 0]))
+        good1 += top5_preds[0, 0].eq(target.data).sum().item()
+        good5 += correct_tensor.sum().item()
+
+    return valid_loss, good1, good5
+
+print(f'Epochs number: {n_epochs}')
+start_time = time.time()
+for epoch in range(n_epochs):
+    start_epoch = time.time()
+
+    train_loss = train_one_epoch(model, train_dataloader, optimizer, criterion, scaler)
+    valid_loss, good1, good5 = validate(model, val_dataloader, criterion)
 
     exp_lr_scheduler.step()
+    if save:
+        torch.save(model.state_dict(), modelName)
+        print("Model saved at end of epoch")
 
-    if save: torch.save(model.state_dict(), modelName); print("Model saved")
+    print(f'Epoch: {epoch + 1} \nTraining Loss: {train_loss / len(train_dataloader.dataset):.5f}')
+    print(f'Validation Loss: {valid_loss / len(val_dataloader.dataset):.5f}')
+    print(f'Accuracy top-1: {100. * good1 / len(val_dataloader.dataset):.1f}% ({good1}/{len(val_dataloader.dataset)})')
+    print(f'Accuracy top-5: {100. * good5 / len(val_dataloader.dataset):.1f}% ({good5}/{len(val_dataloader.dataset)})')
+    print(f'Epoch time {(time.time() - start_epoch) / 60:.1f}m\n')
 
-    """ Validation """
-    model.eval()
-
-    class_correct = list(0. for i in range(200))
-    class_total = list(0. for i in range(200))
-
-    good1 = 0
-    good5 = 0
-    for data, target in val_dataloader:
-        with torch.no_grad():
-            data, target = data.cuda(), target.cuda()
-            output = model(data)
-            loss = criterion(output, target)
-            valid_loss += loss.item() * data.size(0)
-            _, pred = torch.topk(output, 5)
-            # compare predictions to true label
-            correct_tensor = pred[0, 0].eq(target.data.view_as(pred[0, 0]))
-            correct = np.squeeze(correct_tensor.cpu().numpy())
-            # calculate validation accuracy for each object class
-            if batch_size == 1:
-                # top-1 accuracy
-                if pred[0, 0].item() == target.data[0].item(): good1 += 1
-                # top-5 accuracy
-                if pred[0, 0].item() == target.data[0].item()\
-                        or pred[0, 1].item() == target.data[0].item()\
-                        or pred[0, 2].item() == target.data[0].item()\
-                        or pred[0, 3].item() == target.data[0].item()\
-                        or pred[0, 4].item() == target.data[0].item(): good5 += 1
-            else: print('You need to mplement Accuracy stats for batch_size > 1')
-
-    print('Epoch: {} \nTraining Loss: {:.5f} \nValidation Loss: {:.5f}'.format(
-        epoch + 1, train_loss / len(train_dataloader.dataset), valid_loss / len(val_dataloader.dataset)))
-    print('Accuracy top-1: %3.1f%% (%2d/%2d)' % (100. * good1 / len(val_dataloader.dataset),
-                                           good1, len(val_dataloader.dataset)))
-    print('Accuracy top-5: %3.1f%% (%2d/%2d)' % (100. * good5 / len(val_dataloader.dataset),
-                                           good5, len(val_dataloader.dataset)))
-    print('Epoch time %3.1fm' % ((time.time() - start_epoch)/60), '\n')
-
-print('Total time %3.1fm' % ((time.time() - start_time)/60))
+print(f'Total time {(time.time() - start_time) / 60:.1f}m')
